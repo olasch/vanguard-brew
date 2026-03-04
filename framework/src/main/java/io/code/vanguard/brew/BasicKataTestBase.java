58feb86577f87e3c97d160307cb77e6201f9a626
Ola S. Christophersen <olac@netcompany.com>
2026-03-04 11:57:26 +0100

    feat: Checkstyle and ignore more in git

--- framework/src/main/java/io/code/vanguard/brew/BasicKataTestBase.java
+++ framework/src/main/java/io/code/vanguard/brew/BasicKataTestBase.java
@@ -1,7 +1,13 @@
 package io.code.vanguard.brew;
 
 import org.jspecify.annotations.NonNull;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.Arrays;
 import java.util.Optional;
